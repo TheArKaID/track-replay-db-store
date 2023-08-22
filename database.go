7badbff24f4a0712ce5f9df1545d6986d1ce9171
Arifia Kasastra R <arka.progammer@gmail.com>
2023-08-22 11:30:48 +0700

    Adding more setting to database

--- database.go
+++ database.go
@@ -3,24 +3,59 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"os"
+	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
 
 func connect() (clickhouse.Conn, error) {
-	dbhost := os.Getenv("CLICKHOUSE_HOST")
-	if dbhost == "" {
-		// dbhost = "clickhouse-db"
-		dbhost = "localhost"
+	CH_HOST := os.Getenv("CLICKHOUSE_HOST")
+	if CH_HOST == "" {
+		CH_HOST = "localhost"
 	}
+	CH_DB := os.Getenv("CLICKHOUSE_DB")
+	if CH_DB == "" {
+		CH_DB = "default"
+	}
+	CH_USER := os.Getenv("CLICKHOUSE_USER")
+	if CH_USER == "" {
+		CH_USER = "default"
+	}
+	CH_PASS := os.Getenv("CLICKHOUSE_PASS")
+	if CH_PASS == "" {
+		CH_PASS = ""
+	}
+
 	conn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{fmt.Sprintf("%s:%d", dbhost, 9000)},
+		Addr: []string{fmt.Sprintf("%s:%d", CH_HOST, 9000)},
 		Auth: clickhouse.Auth{
-			Database: "default",
-			Username: "default",
-			Password: "",
+			Database: CH_DB,
+			Username: CH_USER,
+			Password: CH_PASS,
 		},
+		DialContext: func(ctx context.Context, addr string) (net.Conn, error) {
+			// dialCount++
+			var d net.Dialer
+			return d.DialContext(ctx, "tcp", addr)
+		},
+		Debug: false,
+		Debugf: func(format string, v ...interface{}) {
+			fmt.Printf(format, v)
+		},
+		Settings: clickhouse.Settings{
+			"max_execution_time": 60,
+		},
+		Compression: &clickhouse.Compression{
+			Method: clickhouse.CompressionLZ4,
+		},
+		DialTimeout:      time.Duration(10) * time.Second,
+		MaxOpenConns:     50,
+		MaxIdleConns:     25,
+		ConnMaxLifetime:  time.Duration(10) * time.Minute,
+		ConnOpenStrategy: clickhouse.ConnOpenInOrder,
+		BlockBufferSize:  10,
 	})
 	return conn, err
 }
@@ -30,13 +65,6 @@ func createTable() {
 	if err != nil {
 		panic(err)
 	}
-
-	// if err := conn.Exec(context.Background(), `DROP TABLE IF EXISTS devices`); err != nil {
-	// 	panic(err)
-	// }
-	// if err := conn.Exec(context.Background(), `DROP TABLE IF EXISTS positions`); err != nil {
-	// 	panic(err)
-	// }
 
 	// Create table based on struct
 	const ddl = `
@@ -87,3 +115,17 @@ func createTable() {
 		panic(err)
 	}
 }
+
+// func xConn() {
+// 	fmt.Println(sql.Drivers())
+// 	db, _ := sql.Open("clickhouse", "clickhouse://default:@localhost:9000/default?dial_timeout=200ms&max_execution_time=60")
+// 	defer db.Close()
+
+// 	row, err := db.Query("SELECT * FROM devices")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	columns, _ := row.Columns()
+// 	fmt.Println(columns)
+// }
