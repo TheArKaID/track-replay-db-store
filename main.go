378481d08b862bdf7c2c6e1c6cf65723e0355042
Arifia Kasastra R <arka.progammer@gmail.com>
2023-08-22 11:31:57 +0700

    Updating main.go progress

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
-	"os"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
@@ -20,108 +19,52 @@ func main() {
 	http.ListenAndServe(":1234", nil)
 }
 
-// Log to a file
-func logToFile(message string) {
-	// Open a file
-	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Close the file
-	defer f.Close()
-
-	// Write to the file, and add new line
-	if _, err := f.WriteString(message + "\n"); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func trackReplay(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method == "POST" {
-
-		// x, _ := io.ReadAll(r.Body)
-		// logToFile(string(x))
-
-		decoder := json.NewDecoder(r.Body)
-
-		var dummy map[string]map[string]interface{}
-
-		err := decoder.Decode(&dummy)
+		var data Data
+		err := dataParser(&data, r.Body)
 		if err != nil {
 			fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		delete(dummy["device"], "id")
-		delete(dummy["position"], "id")
-
-		// Stringify attributes device
-		a, err := json.Marshal(dummy["device"]["attributes"])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// Convert attributes to stringify version
-		dummy["device"]["attributes"] = string(a)
-
-		// Stringify attributes position
-		a, err = json.Marshal(dummy["position"]["attributes"])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// Convert attributes to stringify version
-		dummy["position"]["attributes"] = string(a)
-
-		// Parse Dummy to Data
-		// Convert to JSON
-		b, err := json.Marshal(dummy)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// // Convert JSON to Data
-		var data Data
-		err = json.Unmarshal(b, &data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Print(data.Device.Phone)
-		fmt.Print("\n")
-		fmt.Print(data.Device.Status)
-		fmt.Print("\n")
-		fmt.Print(data.Device.Name)
-		fmt.Print("\n")
 		conn, _ := connect()
-		ctx := clickhouse.Context(context.Background(), clickhouse.WithStdAsync(false))
+		// defer conn.Close()
+		ctx := clickhouse.Context(r.Context(), clickhouse.WithStdAsync(false))
+
 		{
 			conn.Ping(ctx)
 
-			query := fmt.Sprintf(`
-			INSERT INTO devices (name, model, phone, status, contact, category, disabled, unique_id, attributes, expiration_time)
-			VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%t', '%s', '%s', '%s')`, data.Device.Name, data.Device.Model, data.Device.Phone, data.Device.Status, data.Device.Contact, data.Device.Category, data.Device.Disabled, data.Device.UniqueId, data.Device.Attributes, data.Device.ExpirationTime)
-
-			err := conn.AsyncInsert(ctx, query, true)
+			deviceCheckQuery := fmt.Sprintf(`SELECT * FROM devices WHERE unique_id = '%s'`, data.Device.UniqueId)
+			rows, err := conn.Query(ctx, deviceCheckQuery)
 			if err != nil {
 				panic(err)
 			}
 
-			query = fmt.Sprintf(`
-			INSERT INTO positions (speed, valid, course, address, fix_time, network, accuracy, altitude, device_id, latitude, outdated, protocol, longitude, attributes, device_time, server_time)
-			VALUES ('%d', '%t', '%d', '%s', '%s', '%s', '%d', '%d', '%s', '%f', '%t', '%s', '%f', '%s', '%s', '%s')`, data.Position.Speed, data.Position.Valid, data.Position.Course, data.Position.Address, data.Position.FixTime, data.Position.Network, data.Position.Accuracy, data.Position.Altitude, data.Device.UniqueId, data.Position.Latitude, data.Position.Outdated, data.Position.Protocol, data.Position.Longitude, data.Position.Attributes, data.Position.DeviceTime, data.Position.ServerTime)
+			if !rows.Next() {
+				newDeviceQuery := fmt.Sprintf(`
+				INSERT INTO devices (name, model, phone, status, contact, category, disabled, unique_id, attributes, expiration_time)
+				VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%t', '%s', '%s', '%s')`, data.Device.Name, data.Device.Model, data.Device.Phone, data.Device.Status, data.Device.Contact, data.Device.Category, data.Device.Disabled, data.Device.UniqueId, data.Device.Attributes, data.Device.ExpirationTime)
 
-			err = conn.AsyncInsert(ctx, query, true)
+				err = conn.Exec(ctx, newDeviceQuery)
+				if err != nil {
+					panic(err)
+				}
+			}
+			rows.Close()
+
+			newPositionQuery := fmt.Sprintf(`
+			INSERT INTO positions (speed, valid, course, address, fix_time, network, accuracy, altitude, device_id, latitude, outdated, protocol, longitude, attributes, device_time, server_time)
+			VALUES ('%d', '%t', '%d', '%s', '%s', '%v', '%d', '%d', '%s', '%f', '%t', '%s', '%f', '%s', '%s', '%s')`, data.Position.Speed, data.Position.Valid, data.Position.Course, data.Position.Address, data.Position.FixTime, data.Position.Network, data.Position.Accuracy, data.Position.Altitude, data.Device.UniqueId, data.Position.Latitude, data.Position.Outdated, data.Position.Protocol, data.Position.Longitude, data.Position.Attributes, data.Position.DeviceTime, data.Position.ServerTime)
+
+			err = conn.Exec(ctx, newPositionQuery)
 			if err != nil {
 				panic(err)
 			}
 		}
-
+		// fmt.Println(conn.Stats())
 		// readFromDB()
 
 		// w.WriteHeader(http.StatusOK)
@@ -141,38 +84,101 @@ func readFromDB() {
 		panic(err)
 	}
 	// Read from database
-	rows, err := conn.Query(context.Background(), `SELECT * FROM devices order by last_update desc limit 5`)
+	rows, err := conn.Query(context.Background(), `SELECT * FROM positions order by server_time desc limit 5`)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
-	var device Device = Device{}
+	// var device Device = Device{}
+	var position Position = Position{}
 	for rows.Next() {
-		// if err := rows.Scan(
-		// 	&d.Id,
-		// 	&d.Name,
-		// 	&d.Phone,
-		// 	&d.LastUpdate); err != nil {
+		// if err := rows.ScanStruct(&device); err != nil {
 		// 	panic(err)
 		// }
-		if err := rows.ScanStruct(&device); err != nil {
+		// fmt.Printf("Row: %v\n", device)
+
+		if err := rows.Scan(
+			&position.Id,
+			&position.Speed,
+			&position.Valid,
+			&position.Course,
+			&position.Address,
+			&position.FixTime,
+			&position.Network,
+			&position.Accuracy,
+			&position.Altitude,
+			&position.DeviceId,
+			&position.Latitude,
+			&position.Outdated,
+			&position.Protocol,
+			&position.Longitude,
+			&position.Attributes,
+			&position.DeviceTime,
+			&position.ServerTime); err != nil {
 			panic(err)
 		}
-		fmt.Printf("Row: %v\n", device)
+		if err := rows.ScanStruct(&position); err != nil {
+			panic(err)
+		}
+		fmt.Printf("Row: %v\n", position)
 
 	}
 }
 
-// function to format JSON data
-// func formatJSON(data []byte) (string, error) {
-// 	var out bytes.Buffer
-// 	err := json.Indent(&out, data, "", " ")
+func dataParser(data *Data, body io.ReadCloser) error {
+	// x, _ := io.ReadAll(r.Body)
+	// logToFile(string(x))
 
-// 	if err != nil {
-// 		return "", err
-// 	}
+	decoder := json.NewDecoder(body)
 
-// 	d := out.Bytes()
-// 	return string(d), nil
-// }
+	var dummy map[string]map[string]interface{}
+
+	err := decoder.Decode(&dummy)
+	if err != nil {
+		return err
+	}
+
+	delete(dummy["device"], "id")
+	delete(dummy["position"], "id")
+
+	// Stringify attributes device
+	a, err := json.Marshal(dummy["device"]["attributes"])
+	if err != nil {
+		return err
+	}
+	// Convert attributes to stringify version
+	dummy["device"]["attributes"] = string(a)
+
+	// Stringify attributes position
+	a, err = json.Marshal(dummy["position"]["attributes"])
+	if err != nil {
+		return err
+	}
+	// Convert attributes to stringify version
+	dummy["position"]["attributes"] = string(a)
+
+	// Stringify position network
+	a, err = json.Marshal(dummy["position"]["network"])
+	if err != nil {
+		return err
+	}
+	// Convert network to stringify version
+	dummy["position"]["network"] = string(a)
+
+	// Parse Dummy to Data
+	// Convert to JSON
+	b, err := json.Marshal(dummy)
+	if err != nil {
+		return err
+	}
+
+	// // Convert JSON to Data
+	// var data Data
+	err = json.Unmarshal(b, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
